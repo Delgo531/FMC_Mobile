6225b5fbca94e11842e08e7e2eb946dc62f0c43e
Diego Delgado Gómez <20243DS045@utez.edu.mx>
2026-03-14 02:46:50 -0600

    style: Inicio vista LoginScreen

---
+++ app/src/main/java/mx/edu/utez/fmc_mobile/ui/screens/accesControl/login/LoginScreen.kt
@@ -0,0 +1,8 @@
+package mx.edu.utez.fmc_mobile.ui.screens.accesControl.login
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun LoginScreen(navController: NavHostController) {
+}
