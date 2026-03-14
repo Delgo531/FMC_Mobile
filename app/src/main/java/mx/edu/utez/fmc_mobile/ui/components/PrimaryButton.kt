799fe5ac767c3fde601b3f54b105a9a5c51966a5
Diego Delgado Gómez <20243DS045@utez.edu.mx>
2026-03-14 02:43:18 -0600

    style: Creacion componente primary button

---
+++ app/src/main/java/mx/edu/utez/fmc_mobile/ui/components/PrimaryButton.kt
@@ -0,0 +1,36 @@
+package mx.edu.utez.fmc_mobile.ui.components
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import mx.edu.utez.fmc_mobile.ui.theme.AppTypography
+import mx.edu.utez.fmc_mobile.ui.theme.FMC_MobileTheme
+
+@Composable
+fun PrimaryButton(
+    text: String,
+    onClick: () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        shape = RoundedCornerShape(8.dp)
+        ) {
+        Text(text, style = AppTypography.Body, fontWeight = FontWeight.SemiBold)
+    }
+}
+
+@Preview
+@Composable
+fun Preview(){
+    FMC_MobileTheme {
+        PrimaryButton(
+            text = "Iniciar Sesión",
+            onClick = {}
+        )
+    }
+}
