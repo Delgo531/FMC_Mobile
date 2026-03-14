b5ae580c1a17438110d07a36af41c588e4153091
Diego Delgado Gómez <20243DS045@utez.edu.mx>
2026-03-14 02:38:23 -0600

    style: Ajuste de los colores y typo oficial

---
+++ app/src/main/java/mx/edu/utez/fmc_mobile/ui/theme/AppTypography.kt
@@ -0,0 +1,47 @@
+package mx.edu.utez.fmc_mobile.ui.theme
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
+
+object AppTypography {
+
+    val Display = TextStyle(
+        fontFamily = WorkSans,
+        fontSize = 32.sp,
+    )
+
+    val Title = TextStyle(
+        fontFamily = WorkSans,
+        fontSize = 28.sp,
+    )
+
+    val Subtitle = TextStyle(
+        fontFamily = WorkSans,
+        fontSize = 22.sp,
+    )
+
+    val Body = TextStyle(
+        fontFamily = WorkSans,
+        fontSize = 16.sp,
+    )
+
+    val BodyLarge = TextStyle(
+        fontFamily = WorkSans,
+        fontSize = 18.sp,
+    )
+
+    val BodySmall = TextStyle(
+        fontFamily = WorkSans,
+        fontSize = 14.sp,
+    )
+
+    val Caption = TextStyle(
+        fontFamily = WorkSans,
+        fontSize = 12.sp,
+    )
+
+    val Overline = TextStyle(
+        fontFamily = WorkSans,
+        fontSize = 10.sp,
+    )
+}
--- app/src/main/java/mx/edu/utez/fmc_mobile/ui/theme/Color.kt
+++ app/src/main/java/mx/edu/utez/fmc_mobile/ui/theme/Color.kt
@@ -2,10 +2,16 @@ package mx.edu.utez.fmc_mobile.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Primary = Color(0xFFA14DA0)
+val Background = Color(0xFFF9FAFB)
+val Surface = Color(0xFFFFFFFF)
+val Light = Color(0xFFD9B8D9)
+val TextPrimary = Color(0xFF111727)
+val TextSecondary = Color(0xFF6B7289)
+val StatusPending = Color(0xFFFEF9C3)
+val StatusInProgress = Color(0xFFDBEAFE)
+val StatusCompleted = Color(0xFFECDBEC)
+val PendingText = Color(0xFFA16109)
+val InProgressText = Color(0xFF0010D4)
+val CompletedText = Color(0xFFff3435)
